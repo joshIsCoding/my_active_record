9d03b6136e783f1ce5e6518cef51624f57ec7c0e
joshIsCoding <yes_you_do@hotmail.com>
2019-12-08 19:08:29 +0000

    Add explanatory comments for future study

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -3,8 +3,9 @@ class AttrAccessorObject
     names.each do |name|
       define_method(name) { self.instance_variable_get("@#{name.to_s}") } 
       # @-prefix required by method for 'regular instance variables'
-      setter_name = name.to_s + "="
+      setter_name = name.to_s + "=" # setters are called as 'instance_var='
       define_method(setter_name) { |new_val| self.instance_variable_set("@#{name.to_s}", new_val) }
+      # parameters of new method defined as parameters of attached block
     end
   end
 end
