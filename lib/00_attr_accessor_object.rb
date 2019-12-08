5c9a3824ecf90c9cae7c5734d979b0bff303ffd0
joshIsCoding <yes_you_do@hotmail.com>
2019-12-08 19:06:21 +0000

    Create setter methods within AttrAccessorObject::my_attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -3,6 +3,8 @@ class AttrAccessorObject
     names.each do |name|
       define_method(name) { self.instance_variable_get("@#{name.to_s}") } 
       # @-prefix required by method for 'regular instance variables'
+      setter_name = name.to_s + "="
+      define_method(setter_name) { |new_val| self.instance_variable_set("@#{name.to_s}", new_val) }
     end
   end
 end
