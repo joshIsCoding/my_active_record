a85111548197b9a1b93f85e1ea7976c3c391994c
joshIsCoding <yes_you_do@hotmail.com>
2019-12-08 18:55:25 +0000

    Write code to create getter methods within AttrAccessorObject::my_attr_accessor

--- lib/00_attr_accessor_object.rb
+++ lib/00_attr_accessor_object.rb
@@ -1,5 +1,8 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) { self.instance_variable_get("@#{name.to_s}") } 
+      # @-prefix required by method for 'regular instance variables'
+    end
   end
 end
