30c372fb039fc738ed0d2ff4a81230e2448d8cd4
joshIsCoding <yes_you_do@hotmail.com>
2019-12-10 19:29:13 +0000

    Add a Searchable#where module-method for inclusion in SQLObject

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,10 +3,19 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_query = params.keys.map{ |para_name| "#{para_name} = ?" }
+    records = DBConnection.execute(<<-SQL, *params.values)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        #{where_query.join(" AND ")}
+    SQL
+    self.parse_all(records)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
