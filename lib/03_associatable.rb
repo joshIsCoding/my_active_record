26dae6f1f3e9f0ffdfc9c1e777729ecad9c7ee78
joshIsCoding <yes_you_do@hotmail.com>
2019-12-10 21:26:37 +0000

    Write AssocOptions#model_class and #table_name to return the class name constant and ::table_name class method respectively

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,11 +10,11 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize # creates the declared constant class name from the class_name string
   end
 
   def table_name
-    # ...
+    model_class.table_name # calls the class method ::table_name for the target class
   end
 end
 
